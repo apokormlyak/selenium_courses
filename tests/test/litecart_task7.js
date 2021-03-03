a3777211a9f2adb813f279c8847a0694c81765a9
Alisa Pokormlyak <ms.aliska.white@mail.ru>
2021-03-03 10:17:16 +0300

    try3

--- tests/test/litecart_task7.js
+++ tests/test/litecart_task7.js
@@ -2,6 +2,13 @@ const {Key, until, Browser, By} = require('selenium-webdriver');
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome')
 
+// async function isElementPresent(el){
+//     try{
+//         await 
+//     } catch{}
+      
+// }
+
 describe('test7', function(){
 
     let driver;
@@ -18,26 +25,32 @@ describe('test7', function(){
 
     it('test7', async function(){
         await driver.get('http://localhost/litecart/admin/');
-        await driver.findElement(By.name('username'));
-        await driver.findElement(By.name('password'));
-        await driver.findElement(By.css("button[type='submit']")).click();
+        await driver.findElement(By.name('username')).sendKeys('admin', Key.RETURN);
+        await driver.findElement(By.name('password')).sendKeys('admin', Key.RETURN);
+        await driver.findElement(By.css("div.footer button[type='submit']")).click();
+        
+        await driver.wait(() => {
+            until.elementLocated(By.css("div#box-apps-menu-wrapper"));
+            console.log(5, By.css("div#box-apps-menu-wrapper"));
+        }, 50000);
 
-        driver.wait(until.elementLocated(By.css("div#box-apps-menu-wrapper")), 20000);
         //await driver.manage().timeouts().pageLoadTimeout(20000, timeUnit.Seconds);
-
+        
         let rows = driver.findElements(By.css("ul#box-apps-menu li#app-"));
 
         for (row in rows){
-
+            console.log(row);
             let element = row.findElement(By.id('#app-'));
             element.click();
 
-            driver.manage().timeouts().implicityWait(20000);
+            //cdriver.manage().timeouts().implicityWait(20000);
                 let elmsChld = element.findElements(By.css('ul.docs'));
 
             if(elmsChld.length>0){
                 for (elmChld in elmsChld){
                     elmChld.findElements(By.css('ul.docs')).click();
+                    const el = elmChld.findElement(By.css('h1'));
+                    isElementPresent(el);
                 };
 
             };
